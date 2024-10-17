9c2946871b4965a4c0cb8c00f77fb1650cd8491e
ImNaveen <naveendev004@gmail.com>
2024-10-17 08:45:00 +0530

    features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './sections/Header';
+import Hero from './sections/Hero';
 
 function App() {
   return (
     <main className="overflow-hidden">
 
        <Header />
+       <Hero />
     </main>
   );
 }
