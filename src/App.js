99432c5a8c67a14cea2475960730c08349c07f96
ImNaveen <naveendev004@gmail.com>
2024-11-04 16:01:14 +0530

    faq

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './sections/Header';
 import Hero from './sections/Hero';
 import Features from './sections/Features';
 import Pricing from './sections/Pricing';
+import { Faq } from './sections/Faq';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
        <Hero />
        <Features />
        <Pricing />
+       <Faq />
     </main>
   );
 }
